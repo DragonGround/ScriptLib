6b7db4829767f601bacec60bfae818766795f14d
Jeff Lee <jeffomatic@gmail.com>
2023-05-25 14:41:17 -0700

    fix IEnumerator<T> type definition

--- definitions/System/Collections/Generic/Generic.d.ts
+++ definitions/System/Collections/Generic/Generic.d.ts
@@ -1,13 +1,11 @@
-
-
 declare module "System/Collections/Generic" {
-    import { IEnumeratorAny } from "System/Collections"
-
     export interface IEnumerable<T = void> {
         GetEnumerator(): IEnumerator<T>
     }
 
     export interface IEnumerator<T = void> {
-        Current: T
+        get_Current: T
+        MoveNext(): boolean
+        Reset(): void
     }
 }
