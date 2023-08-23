22f8d068f3b8644b25d96beddaca7e0549ec1404
David <st0724@gmail.com>
2023-08-23 11:56:05 -0700

    updated the corresponding .js file

--- onejs/index.js
+++ onejs/index.js
@@ -1,20 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.useEvent = exports.useEventfulState = void 0;
+exports.useRefEvent = exports.useEvent = exports.useEventfulState = void 0;
 var hooks_1 = require("preact/hooks");
 function useEventfulState(obj, propertyName, eventName) {
     var _a = (0, hooks_1.useState)(obj[propertyName]), val = _a[0], setVal = _a[1];
     var _b = (0, hooks_1.useState)({}), updateState = _b[1];
     var forceUpdate = (0, hooks_1.useCallback)(function () { return updateState({}); }, []);
-    eventName = eventName || "On" + String(propertyName) + "Changed";
+    eventName || (eventName = "On".concat(propertyName, "Changed"));
     var addEventFunc = obj["add_".concat(eventName)];
     var removeEventFunc = obj["remove_".concat(eventName)];
     if (!addEventFunc || !removeEventFunc)
         throw new Error("[useEventfulState] The object does not have an event named ".concat(eventName));
-    var onValueChangedCallback = function (v) {
+    function onValueChangedCallback(v) {
         setVal(v);
         forceUpdate();
-    };
+    }
     function removeHandler() {
         removeEventFunc.call(obj, onValueChangedCallback);
     }
@@ -26,17 +26,16 @@ function useEventfulState(obj, propertyName, eventName) {
             unregisterOnEngineReload(removeHandler);
         };
     }, []);
-    var setValWrapper = function (v) {
+    function setValWrapper(v) {
         obj[propertyName] = v;
-    };
+    }
     return [val, setValWrapper];
 }
 exports.useEventfulState = useEventfulState;
 function useEvent(obj, eventName, callback, dependencies) {
     if (dependencies === void 0) { dependencies = []; }
-    var remove = obj["remove_".concat(eventName)].bind(obj);
     function removeHandler() {
-        remove(callback);
+        obj["remove_".concat(eventName)](callback);
     }
     (0, hooks_1.useEffect)(function () {
         obj["add_".concat(eventName)](callback);
@@ -48,3 +47,20 @@ function useEvent(obj, eventName, callback, dependencies) {
     }, dependencies);
 }
 exports.useEvent = useEvent;
+function useRefEvent(ref, eventName, callback, dependencies) {
+    if (dependencies === void 0) { dependencies = []; }
+    function removeHandler() {
+        var obj = ref.current.ve;
+        obj["remove_".concat(eventName)](callback);
+    }
+    (0, hooks_1.useEffect)(function () {
+        var obj = ref.current.ve;
+        obj["add_".concat(eventName)](callback);
+        onEngineReload(removeHandler);
+        return function () {
+            removeHandler();
+            unregisterOnEngineReload(removeHandler);
+        };
+    }, dependencies);
+}
+exports.useRefEvent = useRefEvent;
