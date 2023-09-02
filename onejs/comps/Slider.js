178c76a20f80d46d69d0229c53793f775cc242b3
David <st0724@gmail.com>
2023-09-02 15:40:07 -0700

    Minor tweaks, code style refactor, and kept the original Slider impl as reference

--- onejs/comps/Slider.js
+++ onejs/comps/Slider.js
@@ -25,9 +25,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Slider = void 0;
-var jsx_runtime_1 = require("preact/jsx-runtime");
+exports.OldSlider = exports.Slider = void 0;
 var math_1 = __importDefault(require("math"));
+var preact_1 = require("preact");
 var hooks_1 = require("preact/hooks");
 function Slider(_a) {
     var min = _a.min, max = _a.max, value = _a.value, onChange = _a.onChange, onPointerDown = _a.onPointerDown, onPointerMove = _a.onPointerMove, onPointerUp = _a.onPointerUp, $class = _a.class, trackClass = _a.trackClass, trackStyle = _a.trackStyle, activeTrackClass = _a.activeTrackClass, activeTrackStyle = _a.activeTrackStyle, thumbClass = _a.thumbClass, thumbStyle = _a.thumbStyle, props = __rest(_a, ["min", "max", "value", "onChange", "onPointerDown", "onPointerMove", "onPointerUp", "class", "trackClass", "trackStyle", "activeTrackClass", "activeTrackStyle", "thumbClass", "thumbStyle"]);
@@ -59,8 +59,65 @@ function Slider(_a) {
         var width = trackRef.current.ve.layout.width;
         var ratio = math_1.default.saturate(e.localPosition.x / width);
         activeTrackRef.current.style.width = "".concat(Math.round(ratio * 100), "%");
-        onChange === null || onChange === void 0 ? void 0 : onChange(math_1.default.lerp(min, max, ratio));
+        onChange === null || onChange === void 0 ? void 0 : onChange(math_1.default.lerp(min !== null && min !== void 0 ? min : 0, max !== null && max !== void 0 ? max : 0, ratio));
     }, [onChange, min, max]);
-    return ((0, jsx_runtime_1.jsx)("div", __assign({ ref: trackRef, onPointerDown: handlePointerDown, onPointerMove: handlePointerMove, onPointerUp: handlePointerUp, class: "h-8 justify-center ".concat($class !== null && $class !== void 0 ? $class : "") }, props, { children: (0, jsx_runtime_1.jsx)("div", { class: "h-2 bg-gray-400 rounded-[4px] ".concat(trackClass !== null && trackClass !== void 0 ? trackClass : ""), style: trackStyle, children: (0, jsx_runtime_1.jsx)("div", { ref: activeTrackRef, class: "accented-bg-color h-2 rounded-[4px] ".concat(activeTrackClass !== null && activeTrackClass !== void 0 ? activeTrackClass : ""), style: activeTrackStyle, children: (0, jsx_runtime_1.jsx)("div", { class: "w-6 h-6 default-bg-color border border-gray-400 rounded-full absolute right-0 bottom-1 translate-3 ".concat(thumbClass !== null && thumbClass !== void 0 ? thumbClass : ""), style: thumbStyle }) }) }) })));
+    return ((0, preact_1.h)("div", __assign({ ref: trackRef, class: "h-8 justify-center ".concat($class !== null && $class !== void 0 ? $class : ""), onPointerDown: handlePointerDown, onPointerMove: handlePointerMove, onPointerUp: handlePointerUp }, props),
+        (0, preact_1.h)("div", { class: "h-2 bg-gray-400 rounded-[4px] ".concat(trackClass !== null && trackClass !== void 0 ? trackClass : ""), style: trackStyle },
+            (0, preact_1.h)("div", { ref: activeTrackRef, class: "accented-bg-color h-2 rounded-[4px] ".concat(activeTrackClass !== null && activeTrackClass !== void 0 ? activeTrackClass : ""), style: activeTrackStyle },
+                (0, preact_1.h)("div", { class: "w-6 h-6 default-bg-color border border-gray-400 rounded-full absolute right-0 bottom-1 translate-3 ".concat(thumbClass !== null && thumbClass !== void 0 ? thumbClass : ""), style: thumbStyle })))));
 }
 exports.Slider = Slider;
+var OldSlider = function (_a) {
+    var classProp = _a.class, style = _a.style, value = _a.value, onChange = _a.onChange, _min = _a.min, _max = _a.max;
+    var ref = (0, hooks_1.useRef)();
+    var progressRef = (0, hooks_1.useRef)();
+    var thumbRef = (0, hooks_1.useRef)();
+    var _b = (0, hooks_1.useState)(false), mouseDown = _b[0], setMouseDown = _b[1];
+    var min = _min || 0;
+    var max = _max || 1;
+    var currentValue = value === null || typeof value === "undefined" ? min : value;
+    var currentFraction = (currentValue - min) / (max - min);
+    (0, hooks_1.useEffect)(function () {
+        document.body.addEventListener("MouseMove", handleMouseMove);
+        document.body.addEventListener("MouseUp", handleMouseUp);
+        return function () {
+            document.body.removeEventListener("MouseMove", handleMouseMove);
+            document.body.removeEventListener("MouseUp", handleMouseUp);
+        };
+    }, [mouseDown]);
+    function calculateFromMouseX(clientX) {
+        var rect = ref.current.ve.worldBound;
+        var fraction = (clientX - rect.left) / rect.width;
+        var newValue = (min + fraction * (max - min));
+        return Math.min(Math.max(newValue, min), max);
+    }
+    function processValueChange(newValue) {
+        if (newValue != currentValue) {
+            onChange && onChange(newValue);
+            currentValue = newValue;
+            currentFraction = (currentValue - min) / (max - min);
+            var rect = ref.current.ve.worldBound;
+            progressRef.current.style.width = "".concat(currentFraction * 100, "%");
+            thumbRef.current.style.left = currentFraction * rect.width;
+        }
+    }
+    function handleMouseDown(e) {
+        setMouseDown(true);
+        var newValue = calculateFromMouseX(e.mousePosition.x);
+        processValueChange(newValue);
+    }
+    function handleMouseMove(e) {
+        if (!mouseDown)
+            return;
+        var newValue = calculateFromMouseX(e.mousePosition.x);
+        processValueChange(newValue);
+    }
+    function handleMouseUp() {
+        setMouseDown(false);
+    }
+    return (0, preact_1.h)("div", { class: "h-[30px] justify-center ".concat(classProp), ref: ref, onMouseDown: handleMouseDown, style: style },
+        (0, preact_1.h)("div", { class: "w-full h-[8px] bg-gray-400", style: { borderRadius: 4 } },
+            (0, preact_1.h)("div", { ref: progressRef, class: "accented-bg-color h-[8px] justify-center", style: { width: "".concat(Math.round(currentFraction * 100), "%"), borderRadius: 4 } })),
+        (0, preact_1.h)("div", { ref: thumbRef, class: "w-[24px] h-[24px] default-bg-color absolute rounded-full translate-x-[-10px]" }));
+};
+exports.OldSlider = OldSlider;
--- onejs/comps/Slider.tsx
+++ onejs/comps/Slider.tsx
@@ -1,14 +1,9 @@
-import { Dom } from "OneJS/Dom"
-import {
-    IPointerEvent,
-    PointerDownEvent,
-    PointerMoveEvent,
-    PointerUpEvent,
-} from "UnityEngine/UIElements"
 import math from "math"
-import { JSX } from "preact"
-import { useCallback, useEffect, useRef } from "preact/hooks"
+import { JSX, h } from "preact"
+import { Dom } from "OneJS/Dom"
 import { Style } from "preact/jsx"
+import { useCallback, useEffect, useRef, useState } from "preact/hooks"
+import { IPointerEvent, MouseDownEvent, MouseMoveEvent, PointerDownEvent, PointerMoveEvent, PointerUpEvent } from "UnityEngine/UIElements"
 
 export interface SliderProps extends JSX.VisualElement {
     min?: number
@@ -23,23 +18,7 @@ export interface SliderProps extends JSX.VisualElement {
     thumbStyle?: Style
 }
 
-export function Slider({
-    min,
-    max,
-    value,
-    onChange,
-    onPointerDown,
-    onPointerMove,
-    onPointerUp,
-    class: $class,
-    trackClass,
-    trackStyle,
-    activeTrackClass,
-    activeTrackStyle,
-    thumbClass,
-    thumbStyle,
-    ...props
-}: SliderProps): JSX.Element {
+export function Slider({ min, max, value, onChange, onPointerDown, onPointerMove, onPointerUp, class: $class, trackClass, trackStyle, activeTrackClass, activeTrackStyle, thumbClass, thumbStyle, ...props }: SliderProps): JSX.Element {
     const trackRef = useRef<Dom>()
     const activeTrackRef = useRef<Dom>()
 
@@ -48,75 +27,118 @@ export function Slider({
         activeTrackRef.current.style.width = `${Math.round(ratio * 100)}%`
     }, [min, max, value])
 
-    const handlePointerDown = useCallback(
-        (e: PointerDownEvent) => {
-            e.currentTarget.CapturePointer(e.pointerId)
+    const handlePointerDown = useCallback((e: PointerDownEvent) => {
+        e.currentTarget.CapturePointer(e.pointerId)
+        handlerPointerEvent(e)
+        onPointerDown?.(e)
+    }, [onPointerDown])
+
+    const handlePointerMove = useCallback((e: PointerMoveEvent) => {
+        if (e.currentTarget.HasPointerCapture(e.pointerId)) {
             handlerPointerEvent(e)
-            onPointerDown?.(e)
-        },
-        [onPointerDown]
-    )
+        }
+        onPointerMove?.(e)
+    }, [onPointerMove])
 
-    const handlePointerMove = useCallback(
-        (e: PointerMoveEvent) => {
-            if (e.currentTarget.HasPointerCapture(e.pointerId)) {
-                handlerPointerEvent(e)
-            }
+    const handlePointerUp = useCallback((e: PointerUpEvent) => {
+        if (e.currentTarget.HasPointerCapture(e.pointerId)) {
+            e.currentTarget.ReleasePointer(e.pointerId)
+        }
+        onPointerUp?.(e)
+    }, [onPointerUp])
 
-            onPointerMove?.(e)
-        },
-        [onPointerMove]
-    )
-
-    const handlePointerUp = useCallback(
-        (e: PointerUpEvent) => {
-            if (e.currentTarget.HasPointerCapture(e.pointerId)) {
-                e.currentTarget.ReleasePointer(e.pointerId)
-            }
-
-            onPointerUp?.(e)
-        },
-        [onPointerUp]
-    )
-
-    const handlerPointerEvent = useCallback(
-        (e: IPointerEvent) => {
-            const width = trackRef.current.ve.layout.width
-            const ratio = math.saturate(e.localPosition.x / width)
-            activeTrackRef.current.style.width = `${Math.round(ratio * 100)}%`
-            onChange?.(math.lerp(min, max, ratio))
-        },
-        [onChange, min, max]
-    )
+    const handlerPointerEvent = useCallback((e: IPointerEvent) => {
+        const width = trackRef.current.ve.layout.width
+        const ratio = math.saturate(e.localPosition.x / width)
+        activeTrackRef.current.style.width = `${Math.round(ratio * 100)}%`
+        onChange?.(math.lerp(min ?? 0, max ?? 0, ratio))
+    }, [onChange, min, max])
 
     return (
-        <div
-            ref={trackRef}
-            onPointerDown={handlePointerDown}
-            onPointerMove={handlePointerMove}
-            onPointerUp={handlePointerUp}
-            class={`h-8 justify-center ${$class ?? ""}`}
-            {...props}
-        >
-            <div
-                class={`h-2 bg-gray-400 rounded-[4px] ${trackClass ?? ""}`}
-                style={trackStyle}
-            >
-                <div
-                    ref={activeTrackRef}
-                    class={`accented-bg-color h-2 rounded-[4px] ${
-                        activeTrackClass ?? ""
-                    }`}
-                    style={activeTrackStyle}
-                >
-                    <div
-                        class={`w-6 h-6 default-bg-color border border-gray-400 rounded-full absolute right-0 bottom-1 translate-3 ${
-                            thumbClass ?? ""
-                        }`}
-                        style={thumbStyle}
-                    />
+        <div ref={trackRef} class={`h-8 justify-center ${$class ?? ""}`} onPointerDown={handlePointerDown} onPointerMove={handlePointerMove} onPointerUp={handlePointerUp} {...props}>
+            <div class={`h-2 bg-gray-400 rounded-[4px] ${trackClass ?? ""}`} style={trackStyle}>
+                <div ref={activeTrackRef} class={`accented-bg-color h-2 rounded-[4px] ${activeTrackClass ?? ""}`} style={activeTrackStyle}>
+                    <div class={`w-6 h-6 default-bg-color border border-gray-400 rounded-full absolute right-0 bottom-1 translate-3 ${thumbClass ?? ""}`} style={thumbStyle} />
                 </div>
             </div>
         </div>
     )
+}
+
+export type OldSliderProps = {
+    class?: string
+    style?: Style
+    value?: number
+    onChange?: (value: number) => void
+    min?: number
+    max?: number
+}
+
+/**
+ * OldSlider is included here for DOM manipulation reference. For practical use, opt for Slider instead.
+ * Slider is more modular and uses PointerCapture for better tracking.
+ */
+export const OldSlider = ({ class: classProp, style, value, onChange, min: _min, max: _max }: OldSliderProps) => {
+    const ref = useRef<Dom>()
+    const progressRef = useRef<Dom>()
+    const thumbRef = useRef<Dom>()
+
+    const [mouseDown, setMouseDown] = useState(false)
+
+    const min = _min || 0
+    const max = _max || 1
+
+    let currentValue = value === null || typeof value === "undefined" ? min : value
+    let currentFraction = (currentValue - min) / (max - min)
+
+    useEffect(() => {
+        document.body.addEventListener("MouseMove", handleMouseMove)
+        document.body.addEventListener("MouseUp", handleMouseUp)
+        return () => {
+            document.body.removeEventListener("MouseMove", handleMouseMove)
+            document.body.removeEventListener("MouseUp", handleMouseUp)
+        }
+    }, [mouseDown])
+
+    function calculateFromMouseX(clientX) {
+        const rect = ref.current.ve.worldBound
+        const fraction = (clientX - rect.left) / rect.width
+        const newValue = (min + fraction * (max - min))
+        return Math.min(Math.max(newValue, min), max)
+    }
+
+    function processValueChange(newValue: number) {
+        if (newValue != currentValue) {
+            onChange && onChange(newValue)
+            currentValue = newValue
+            currentFraction = (currentValue - min) / (max - min)
+            const rect = ref.current.ve.worldBound
+            progressRef.current.style.width = `${currentFraction * 100}%`
+            thumbRef.current.style.left = currentFraction * rect.width
+        }
+    }
+
+    function handleMouseDown(e: MouseDownEvent) {
+        setMouseDown(true)
+        const newValue = calculateFromMouseX(e.mousePosition.x)
+        processValueChange(newValue)
+    }
+
+    function handleMouseMove(e: MouseMoveEvent) {
+        if (!mouseDown) return
+        const newValue = calculateFromMouseX(e.mousePosition.x)
+        processValueChange(newValue)
+    }
+
+    function handleMouseUp() {
+        setMouseDown(false)
+    }
+
+    return <div class={`h-[30px] justify-center ${classProp}`} ref={ref} onMouseDown={handleMouseDown} style={style}>
+        <div class={`w-full h-[8px] bg-gray-400`} style={{ borderRadius: 4 }}>
+            <div ref={progressRef} class={`accented-bg-color h-[8px] justify-center`} style={{ width: `${Math.round(currentFraction * 100)}%`, borderRadius: 4 }}>
+            </div>
+        </div>
+        <div ref={thumbRef} class={`w-[24px] h-[24px] default-bg-color absolute rounded-full translate-x-[-10px]`} />
+    </div>
 }
